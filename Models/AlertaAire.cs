dcbf78128e79af8c7e50d2b9c83d8e40e3b4425a
anthonyservellon07-droid <anthonyservellon07@gmail.com>
2026-05-18 23:18:45 -0600

    Update AlertaAire.cs

--- Models/AlertaAire.cs
+++ Models/AlertaAire.cs
@@ -1,11 +1,11 @@
 ﻿namespace CalidadAireAPI.Models
 {
-    // Aca guardamos las alertas que se disparan según los niveles de la OMS
+    // Aca guardao las alertas que se disparan según los niveles de la OMS......................
     public class AlertaAire
     {
         public int Id { get; set; }
         public int SensorId { get; set; }
-        public string Nivel { get; set; } = string.Empty; // Leve, Moderada, etc.
+        public string Nivel { get; set; } = string.Empty; // Leve --- Moderada ......................
         public string Mensaje { get; set; } = string.Empty;
         public DateTime FechaHora { get; set; } = DateTime.Now;
 
