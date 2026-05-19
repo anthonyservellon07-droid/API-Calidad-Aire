27d6da78be676fa06f5ef745b3ad7b0009a2c3cf
anthonyservellon07-droid <anthonyservellon07@gmail.com>
2026-05-18 23:18:15 -0600

    Update AireDbContext.cs

--- Models/AireDbContext.cs
+++ Models/AireDbContext.cs
@@ -2,22 +2,21 @@
 
 namespace CalidadAireAPI.Models
 {
-    // Clase para manejar la conexion a la base de datos
+    // Clase para manejar la conexion a la base de datos..................................
     public class AireDbContext : DbContext
     {
         public AireDbContext(DbContextOptions<AireDbContext> options) : base(options)
         {
         }
 
-        // Tablas mapeadas a nuestros modelos
+        // Tablas mapeadas a los modelos ............................................
         public DbSet<SensorCalidadAire> SensorCalidadAire { get; set; }
         public DbSet<LecturaAire> LecturaAire { get; set; }
         public DbSet<AlertaAire> AlertaAire { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Dejo esto por si el profe pide configuraciones extra despues
-            // pero por ahora EF Core mapea bien con los nombres de las propiedades (Id, SensorId)
+            
             base.OnModelCreating(modelBuilder);
         }
     }
