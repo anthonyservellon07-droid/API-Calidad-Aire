50b73fabcc06cde6c4d337400c22c06dc69d71d6
anthonyservellon07-droid <anthonyservellon07@gmail.com>
2026-05-18 23:21:33 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Conexion a Base de Datos
+// 1.1.------ Conexion a Base de Datos ..............................................................
 builder.Services.AddDbContext<AireDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 2. Configuracion de Seguridad JWT
+// 2.1 Configuracion de Seguridad JWT ...............................................................
 var llaveSecreta = "EstaEsMiClaveSuperSecretaDelParcial2026_NoRobar";
 var key = Encoding.ASCII.GetBytes(llaveSecreta);
 
@@ -31,20 +31,20 @@ builder.Services.AddAuthentication(x => {
 
 builder.Services.AddControllers();
 
-// 3. Swagger BÁSICO (Para cumplir el requisito sin que NET 10 explote)
+// 3.1 Swagger ........................................................................................
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Para la API del clima
+// Para la API del clima ..................................................................
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// 4. Activar Swagger
+// 4.1 Activar Swagger..................................................................
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// app.UseHttpsRedirection(); // Se queda comentado para evitar el 404 de antes
+// app.UseHttpsRedirection(); 
 app.UseAuthentication();
 app.UseAuthorization();
 
