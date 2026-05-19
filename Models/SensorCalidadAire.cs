5f6f9c9558cb3707e92da3bee6edcd52fbcde0df
anthonyservellon07-droid <anthonyservellon07@gmail.com>
2026-05-18 23:19:44 -0600

    Update SensorCalidadAire.cs

--- Models/SensorCalidadAire.cs
+++ Models/SensorCalidadAire.cs
@@ -1,16 +1,16 @@
 ﻿namespace CalidadAireAPI.Models
 {
-    // Clase principal para registrar los sensores físicos de la planta
+    // Clase principal para registrar los sensores físicos - planta ......................
     public class SensorCalidadAire
     {
         public int Id { get; set; }
         public string Ubicacion { get; set; } = string.Empty;
         public string TipoGas { get; set; } = string.Empty;
 
-        // Lo ponemos en true por defecto para no batallar al crear nuevos
+
         public bool Estado { get; set; } = true;
 
-        // Navegación para Entity Framework
+        // Navegación para el Entity Framework............................................
         public List<LecturaAire> Lecturas { get; set; } = new();
         public List<AlertaAire> Alertas { get; set; } = new();
     }
